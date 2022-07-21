944452bf45b401a8c5625c02f86c1fdb6170e182
Dmitry Kozmin <kozmin4ik@gmail.com>
2022-07-21 16:05:40 +0300

    Made some chages

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import cgi
+import sys
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from method_db import add_part, conection, cursor
 
@@ -5,34 +7,59 @@ from method_db import add_part, conection, cursor
 class echoHANDLER(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path.endswith('/senddata'):
-            self.send_response(200)
-            self.send_header('Content-type',  'text/html; charset=utf-8')
-            self.end_headers()
+            self._set_response()
             site = open('site.html', 'rb')
             site = site.read()
             self.wfile.write(site)
 
+        if self.path.endswith('/sendfile'):
+            self._set_response()
+            site = open('send_file.html', 'rb')
+            site = site.read()
+            self.wfile.write(site)
+
     def do_POST(self):
-        content_length = int(self.headers.get('content-length'), 0)
-        insert_data = self.rfile.read(content_length).decode('utf-8')
-        self.send_response(200)
-        self.end_headers()
-        file = open('result.html', 'rb')
-        file = file.read()
-        self.wfile.write(file)
-        self.add_to_DB(insert_data)
+        try:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.send_header('Location', self.path)
+            self.end_headers()
+            ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
+            pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+            content_len = int(self.headers.get('Content-length'))
+            pdict['CONTENT-LENGTH'] = content_len
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                if self.path.endswith('/sendfile'):
+                    insert_data = fields.get('file')
+                    insert_data = insert_data[0].decode('utf-8').splitlines()
+                    self.add_to_DB(insert_data)
+
+                elif self.path.endswith('/senddata'):
+                    categories = fields.get('category')
+                    key = fields.get('tel')
+                    timeout = fields.get('timeout')
+                    add_part(conection, cursor, categories=categories, key=key[0], timeout=timeout[0])
+            file = open('result.html', 'rb')
+            site = file.read()
+            self.wfile.write(site)
+        except:
+            self.send_error(404, "{}".format(sys.exc_info()[0]))
+            print(sys.exc_info())
 
     def add_to_DB(self, insert_data):
-        insert_data = insert_data.split('&')
-        result = []
-        categories = []
-        for i in insert_data:
-            i = i.split('=')
-            if i[0] == 'category':
-                categories.append(i[1])
-            else:
-                result.append(i[1])
-        add_part(conection, cursor, categories=categories, key=result[0], timeout=result[1])
+        header = insert_data[0].split(',')
+        data = insert_data[1].split(',')
+        data[0] = data[0].split(';')
+        categories = data[header.index('categories')]
+        key = data[header.index('phone')]
+        timeout = data[header.index('timeout')]
+        add_part(conection, cursor, categories=categories, key=key, timeout=timeout)
+
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
 
 
 def main():
